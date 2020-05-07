9f02ece9e7eae939d9095898345ea06b2a2069ec
Rafael1993 <rafamendesufpr@gmail.com>
2020-05-07 13:24:03 -0300

    Update implementaton

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,17 +16,20 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("===Test 1: seller findById======");
+		
 		Seller seller = sellerDao.findById(3);
-		
-		
 		System.out.println(seller);
+		System.out.println();
+		
 		System.out.println("===Test 2: seller findByDepartment======");
+		
 		Department department = new Department (2 , null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			
 			System.out.println(obj);
 		}
+		System.out.println();
 		
 		System.out.println("===Test 3: seller findAll======");
 		
@@ -35,11 +38,21 @@ public class Program {
 			
 			System.out.println(obj);
 		}
+		System.out.println();
 		
 		System.out.println("===Test 4: seller Insert======");
 		Seller newSeller = new Seller(null , "Greg" , "greg@gmail.com" , new Date()  , 4000.0 , department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println();
+		
+		System.out.println("===Test 5: seller Insert======");
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update xomplete");
+		
 	}
 
 }
